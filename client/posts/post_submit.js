ef3e0a15c87771f244a273a6f8464e83523bcb49
tete1230 <410360957@qq.com>
2016-07-19 15:56:34 +0800

    错误钱

--- client/posts/post_submit.js
+++ client/posts/post_submit.js
@@ -7,7 +7,16 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(error, result) {
+      // 显示错误信息并退出
+      if (error)
+        return alert(error.reason);
+
+      // 显示结果，跳转页面
+      if (result.postExists)
+        alert('This link has already been posted 该链接已神一般的存在！');
+    
+      Router.go('postPage', {_id: result._id});
+    });
   }
 });
