bf77b522155c36f6d51e1796805b219505811c1b
tete1230 <410360957@qq.com>
2016-07-21 16:35:49 +0800

    下一步将 postSubmitErrors Session 会话对象绑在表单上。

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,6 +1,6 @@
 Template.postItem.helpers({
   ownPost: function() {
-    return this.userId === Meteor.userId();
+    return this.userId == Meteor.userId();
   },
   domain: function() {
     var a = document.createElement('a');
